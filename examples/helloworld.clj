029b00ba654f43bd049ac8a0e8d2fee2f67de158
Brent Millare <brent.millare@gmail.com>
2017-06-25 22:21:35 -0400

    added stub in src directory

--- examples/helloworld.clj
+++ examples/helloworld.clj
@@ -1,3 +1,3 @@
-":"; mkdir -p ~/.dj; if [ ! -e "$HOME/.dj/run" ]; cd $HOME/.dj; curl -O 'https://raw.githubusercontent.com/bmillare/dj3/master/bin/run'; cd -; fi; exec sh ~/.dj/run $0 $@
+":"; mkdir -p ~/.dj; if [ ! -e "$HOME/.dj/run" ]; then cd $HOME/.dj; curl -O 'https://raw.githubusercontent.com/bmillare/dj3/master/bin/run'; cd -; fi; exec sh ~/.dj/run $0 $@
 (prn "clojure runs baybe" *command-line-args*)
 (println (System/getProperty "user.dir"))
