c1aadf7879366a59050696675f076c27c7290f81
Brent Millare <brent.millare@gmail.com>
2017-11-29 00:32:05 -0500

    java 9 attempt fix 2

--- src/dj/init/cidernrepl.clj
+++ src/dj/init/cidernrepl.clj
@@ -1,7 +1,18 @@
 (ns dj.init.cidernrepl)
 
 (require '[cemerick.pomegranate :as cp]
-         '[cemerick.pomegranate.aether :as a])
+         '[cemerick.pomegranate.aether :as a]
+         '[dynapath.dynamic-classpath :as dcp])
+
+(-> (Thread/currentThread)
+    (.setContextClassLoader (clojure.lang.RT/baseLoader)))
+
+;; Not sure if this is necessary
+#_ (extend jdk.internal.loader.ClassLoaders$AppClassLoader
+  dcp/DynamicClasspath
+  (assoc dcp/base-readable-addable-classpath
+    :classpath-urls #(seq (.getURLs %))
+    :can-add? (constantly false)))
 
 (cp/add-dependencies
  :coordinates '[[refactor-nrepl "2.3.1"]
@@ -12,6 +23,7 @@
 (require '[clojure.tools.nrepl.server]
          '[cider.nrepl])
 
-(println "starting cider nrepl server on port 9000")
-(clojure.tools.nrepl.server/start-server :port 9000 :handler cider.nrepl/cider-nrepl-handler)
-(println "running cider nrepl server on port 9000")
+(let [port 9001]
+  (println "starting cider nrepl server on port" port)
+  (clojure.tools.nrepl.server/start-server :port port :handler cider.nrepl/cider-nrepl-handler)
+  (println "running cider nrepl server on port" port))
